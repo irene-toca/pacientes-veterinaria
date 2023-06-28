5e8bb2dbbe422fa09a2d31765b84933cbce6e371
Irene Toca <irene.devops@gmail.com>
2023-06-28 19:19:09 +0100

    Add form and patients list

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,41 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl " >
+
+
+        <p className="font-bold mb-3 text-gray-700 uppercase ">Nombre: {''}
+          <span className="font-normal normal-case "> Hook</span>
+        </p>
+
+
+        <p className="font-bold mb-3 text-gray-700 uppercase ">Propietario: {''}
+          <span className="font-normal normal-case "> Irene </span>
+        </p>
+
+
+        <p className="font-bold mb-3 text-gray-700 uppercase "> Email: {''}
+          <span className="font-normal normal-case "> correo@correo.com </span>
+        </p>
+
+
+
+        <p className="font-bold mb-3 text-gray-700 uppercase ">Fecha Alta: {''}
+          <span className="font-normal normal-case "> 10 Diciembre de 2022 </span>
+        </p>
+
+
+
+        <p className="font-bold mb-3 text-gray-700 uppercase "> Síntomas : {''}
+          <span className="font-normal normal-case "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere praesentium nesciunt amet cum eius soluta temporibus quod, at, reiciendis error pariatur? Vitae maiores et velit quam est aut explicabo. Architecto.</span>
+        </p>
+
+
+
+
+      </div>
+  )
+}
+
+export default Paciente
